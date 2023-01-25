8d7923b62bd29952afc047053e15b8933881d7f8
yungukbae <yungukbae@gmail.com>
2023-01-25 21:17:01 +0900

    feat: add tailwindcss:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "@/styles/Home.module.css";
-import { testImg } from "assets";
 
 export default function Home() {
   return (
@@ -12,8 +9,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Image src={`${testImg.src}`} alt="test" width={500} height={500} />
-      <main className={styles.main}></main>
     </>
   );
 }
