eec8591b82ee1a31985d82708962962b63645027
yungukbae <yungukbae@gmail.com>
2023-01-29 21:00:48 +0900

    feat: ui, ui test

--- src/pages/__test__/index.test.tsx
+++ src/pages/__test__/index.test.tsx
@@ -2,11 +2,12 @@ import Home from "@/pages/index";
 import { render, screen } from "@testing-library/react";
 
 describe("Todo Test", () => {
-  it("ui test", () => {
+  it("ui test", async () => {
     render(<Home />);
-
-    screen.findAllByText(/todo list/i);
-    screen.findByRole("textbox");
-    screen.findByRole("button");
+    await screen.findByText(/todo list/i);
+    await screen.findByText(/first task/i);
+    await screen.findByRole("textbox");
+    const checkEl = await screen.findByRole("checkbox");
+    expect(checkEl).not.toBeChecked();
   });
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,23 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="bg-slate-500 mx-auto h-screen">test</div>
+      <div className="bg-slate-500 h-screen pt-48">
+        <div className="mx-auto w-60 ">
+          <div className="w-60 bg-gray-400 rounded-lg text-center mb-8">
+            Todo List
+          </div>
+          <div>
+            <input type="text" className="w-60 rounded-lg mb-2" />
+            <hr className="mb-5" />
+          </div>
+          <div>
+            <div className="bg-gray-200 rounded-lg flex justify-between px-5">
+              <div className="truncate">first task</div>
+              <input type="checkbox" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
