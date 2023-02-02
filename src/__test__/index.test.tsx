7b0eab254bbd942bf0bb78103fbae8f4c36ada08
yungukbae <yungukbae@gmail.com>
2023-02-02 21:49:40 +0900

    feat: first TDD Todo App

--- src/__test__/index.test.tsx
+++ src/__test__/index.test.tsx
@@ -3,15 +3,21 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe("Todo Test", () => {
-  it("ui test", async () => {
-    render(<Home />);
-  });
-
   it("input test", async () => {
     render(<Home />);
-  });
-
-  it("checkbox Test", async () => {
-    render(<Home />);
+    const inputEl = await screen.getByRole("textbox");
+    const addBtn = await screen.getByRole("button", { name: "addBtn" });
+    await userEvent.type(inputEl, "test task");
+    await userEvent.click(addBtn);
+    expect(inputEl).toHaveValue("");
+    await screen.getByText(/test task/i);
+    const taskTxt = await screen.getByText(/test task/i);
+    const checkBox = await screen.getAllByRole("checkbox");
+    const delBtn = await screen.getAllByRole("button");
+    await userEvent.click(checkBox[1]);
+    expect(checkBox[1]).toBeChecked();
+    expect(taskTxt).toHaveClass("line-through");
+    await userEvent.click(delBtn[2]);
+    expect(taskTxt).not.toBeInTheDocument();
   });
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,52 @@
 import Head from "next/head";
+import { useReducer, useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
+interface Prev {
+  id: string;
+  task: string;
+  check: boolean;
+}
+interface ActionType extends Prev {
+  type: "add" | "del" | "mod";
+}
+
+const initReducer = [{ id: "test", task: "first task", check: false }];
+
+const reducer = (prev: Prev[], action: ActionType) => {
+  const type = action.type;
+  switch (type) {
+    case "add":
+      return [...prev, action];
+    case "del": {
+      const nextArr = prev.filter((x) => x.id !== action.id);
+      return nextArr;
+    }
+    case "mod": {
+      const mod = prev.map((x) => {
+        if (x.id === action.id) {
+          return action;
+        } else {
+          return x;
+        }
+      });
+      return mod;
+    }
+    default:
+      return prev;
+  }
+};
 
 export default function Home() {
+  const [task, dispatch] = useReducer(reducer, initReducer);
+  const isRef = useRef<HTMLInputElement>(null);
+
+  const handleTask = (body: ActionType) => {
+    dispatch(body);
+    if (isRef.current && body.type === "add") {
+      isRef.current.value = "";
+    }
+  };
+
   return (
     <>
       <Head>
@@ -9,7 +55,65 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="bg-slate-500 h-screen pt-48"></div>
+      <div className="flex flex-col items-center bg-slate-500 h-screen pt-48 ">
+        <div>
+          <input type="text" ref={isRef} />
+          <button
+            name="addBtn"
+            onClick={() =>
+              handleTask({
+                type: "add",
+                task: isRef.current!.value,
+                id: uuidv4(),
+                check: false,
+              })
+            }
+          >
+            addBtn
+          </button>
+        </div>
+        <div className="flex flex-col">
+          {task.map((row) => {
+            return (
+              <div key={row.id} className={"flex flex-row "}>
+                <div className={row.check ? "line-through" : ""}>
+                  {row.task}
+                </div>
+                <div>
+                  <input
+                    type="checkbox"
+                    name="taskCheckBox"
+                    defaultChecked={row.check}
+                    onClick={() =>
+                      handleTask({
+                        type: "mod",
+                        task: row.task,
+                        id: row.id,
+                        check: !row.check,
+                      })
+                    }
+                  />
+                </div>
+                <div>
+                  <button
+                    name="delBtn"
+                    onClick={() =>
+                      handleTask({
+                        type: "del",
+                        task: row.task,
+                        id: row.id,
+                        check: row.check,
+                      })
+                    }
+                  >
+                    del
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
